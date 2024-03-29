f5febf0992616321a9a9e304e6c92253577bc5d6
francoromeo <francoromeo5012@gmail.com>
2024-03-28 23:46:28 -0300

    js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const divtoShow = 'nav .menu';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.m-trigger');
+
+divTrigger.addEventListener('click', () =>{
+  setTimeout(() =>{
+    if (!divPopup.classList.contains('show')){
+      divPopup.classList.add('show');
+      document.body.classList.add('menu-visible')
+    }
+  }, 250);
+})
